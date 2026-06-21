190b379649180ad1babab137a0973d2c47410e9e
Ram Kumar R <rockyramkumar128@gmail.com>
2026-06-22 00:13:31 +0530

    Add files via upload

---
+++ Java_FSE/Deepskilling/Week1/JUnit/JUnit_Basic Testing Exercises/JUnitExercise4/src/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,56 @@
+package com.cognizant.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+
+        System.out.println("Setup Method Executed");
+
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+
+        System.out.println("Teardown Method Executed");
+
+        calculator = null;
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void testMultiply() {
+
+        // Arrange
+        int a = 5;
+        int b = 4;
+
+        // Act
+        int result = calculator.multiply(a, b);
+
+        // Assert
+        assertEquals(20, result);
+    }
+}
