44ea06916e3bad8d4b316c66abeee8519084f6e5
Ram Kumar R <rockyramkumar128@gmail.com>
2026-06-24 12:28:04 +0530

    Add files via upload

---
+++ Java_FSE/Deepskilling/Week2/Spring Core and Maven/Exercise4_MavenConfiguration/src/main/java/com/library/MavenConfigurationDemo.java
@@ -0,0 +1,11 @@
+package com.library;
+
+public class MavenConfigurationDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Spring Maven Configuration Successful");
+
+    }
+}
